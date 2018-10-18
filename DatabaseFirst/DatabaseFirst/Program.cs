59b49c32480fb381fa515b44b81a4ef12982a6c4
omar soto <alejandro.1915@hotmail.com>
2018-10-18 00:33:25 -0700

    databaseFirst
    The DataBaseFirstDemo Project

---
+++ DatabaseFirst/DatabaseFirst/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DatabaseFirstDemoEntities1();
+            var post = new Post
+            {
+                Body = "Body",
+                DataPublished = DateTime.Now,
+                Title = "Title",
+                TestColumn = "test",
+                PostID = 2
+            };
+
+            context.Post.Add(post);
+            context.SaveChanges();
+        }
+    }
+}
